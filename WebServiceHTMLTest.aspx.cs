00b3cd96d382bd22a2d5bb3e8a782188d2f8b088
AlexanderUsov <alexander.usov@gmail.com>
2017-03-11 11:29:01 +0300

    Second commit

--- WebServiceHTMLTest.aspx.cs
+++ WebServiceHTMLTest.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Script.Serialization;
-//ok
+//ok my friends
 public partial class WebServiceHTMLTest : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
