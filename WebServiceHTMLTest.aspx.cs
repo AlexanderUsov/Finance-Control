14ed9c7cb7215cd01a79e38d349757d585851692
AlexanderUsov <alexander.usov@gmail.com>
2017-03-11 11:30:29 +0300

    3d commitment

--- WebServiceHTMLTest.aspx.cs
+++ WebServiceHTMLTest.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Script.Serialization;
-//ok my friends
+//ok my friends kkk
 public partial class WebServiceHTMLTest : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
